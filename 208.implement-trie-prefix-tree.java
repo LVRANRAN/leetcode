01b24b5d691872667470144e2389776ebd76375c
LVRANRAN <m13871193221@163.com>
2019-11-17 16:37:20 -0500

    Implement Trie (Prefix Tree)

---
+++ 208.implement-trie-prefix-tree.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=208 lang=java
+ *
+ * [208] Implement Trie (Prefix Tree)
+ */
+
+// @lc code=start
+class TrieNode {
+    public TrieNode[] children = new TrieNode[26];
+    public char c;
+    public boolean value;
+    public TrieNode() {
+    }
+    public TrieNode(char c) {
+        this.c = c;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+    /** Initialize your data structure here. */
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                cur.children[index] = new TrieNode(word.charAt(i));
+            }
+            cur = cur.children[index];
+        }
+        cur.value = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                return false;
+            }
+            cur = cur.children[index];
+        }
+        if(!cur.value) return false;
+        return true;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int index = prefix.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                return false;
+            }
+            cur = cur.children[index];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+// @lc code=end
+
