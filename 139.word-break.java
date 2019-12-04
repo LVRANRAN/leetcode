dbb35e3a167760eb9b16523133b2617c2f6550f8
LVRANRAN <m13871193221@163.com>
2019-12-04 11:46:45 -0500

    Word Break

---
+++ 139.word-break.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+ * @lc app=leetcode id=139 lang=java
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String str : wordDict) {
+            set.add(str);
+        }
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] == true && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
